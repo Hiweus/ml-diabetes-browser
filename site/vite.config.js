3fa99997b5074a9283da45395f9169d2b55634dc
Andre Alves Leocadio <andre.leocadio@getrak.com.br>
2024-11-28 12:52:20 -0300

    fix: corrige deploy de pasta

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base: process.env.PUBLIC_URL,
   plugins: [react()],
 })
